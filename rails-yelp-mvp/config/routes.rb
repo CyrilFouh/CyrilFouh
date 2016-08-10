f0607cbbe8b2b6288287120b231c754427dbffe3
Cyril Fougeres <cyril.fougeres@gmail.com>
2016-08-10 12:45:42 +0200

    rails new

--- rails-yelp-mvp/config/routes.rb
+++ rails-yelp-mvp/config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'answer' => 'pages#answer'
-
-  root 'pages#ask'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
